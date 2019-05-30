8d130c3e007561d83c920ea14607cedd85a7647f
kyzas <zenyz.cs@gmail.com>
2019-05-30 10:01:33 +0000

    fix import for python 2

--- scake/__init__.py
+++ scake/__init__.py
@@ -2,6 +2,13 @@
 
 __version__ = "0.1.0"
 
-from .scake import Scake
-from .auto import AutoScake
-from .structure import ScakeDict
+import sys
+major, minor, micro, _, _ = sys.version_info
+if major == 2:
+    from scake import Scake
+    from auto import AutoScake
+    from structure import ScakeDict
+else:
+    from .scake import Scake
+    from .auto import AutoScake
+    from .structure import ScakeDict
