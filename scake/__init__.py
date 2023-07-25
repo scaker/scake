9a92ad5e3e645e9763b0088505f73457ec043bd0
minhng92 <minhng92@gmail.com>
2023-07-25 09:35:01 +0000

    expose SckLog class

--- scake/__init__.py
+++ scake/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 __version__ = "0.3.0"
+from .libcore import SckLog
 from .scake import Scake
 
 __all__ = [
     "Scake",
+    "SckLog",
 ]
--- scake/libcore/__init__.py
+++ scake/libcore/__init__.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
 from . import config_loader, sck_format, sck_graph, sck_log, sck_singleton
+from .sck_log import SckLog
 
-__all__ = ["config_loader", "sck_graph", "sck_format", "sck_singleton", "sck_log"]
+__all__ = [
+    "SckLog",
+    "config_loader",
+    "sck_graph",
+    "sck_format",
+    "sck_singleton",
+    "sck_log",
+]
